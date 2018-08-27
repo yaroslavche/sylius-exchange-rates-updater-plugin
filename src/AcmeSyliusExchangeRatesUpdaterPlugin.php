b86a036bf48e1990bb79bd09a9de837b0fa51656
yaroslavche <yaroslav429@gmail.com>
2018-08-27 13:46:05 +0300

    Refactor names
    SyliusExamplePlugin -> SyliusExchangeRatesUpdaterPlugin
    change autoload in composer.json, composer dumpautoload
    bin/console cache:clear

--- src/AcmeSyliusExchangeRatesUpdaterPlugin.php
+++ src/AcmeSyliusExchangeRatesUpdaterPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Acme\SyliusExchangeRatesUpdaterPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class AcmeSyliusExchangeRatesUpdaterPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }

12d9bca1a3e00e7beac425fa4e7b8195cb66e909
yaroslavche <yaroslav429@gmail.com>
2018-08-27 14:53:07 +0300

    Added command
    sylius:exchange-rates:update
    Dummy implementation, need work

---
+++ src/Command/SyliusExchangeRatesUpdateCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acme\SyliusExchangeRatesUpdaterPlugin\Command;
+
+use Acme\SyliusExchangeRatesUpdaterPlugin\Service\ExchangeRatesUpdater;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class SyliusExchangeRatesUpdateCommand extends Command
+{
+    protected static $defaultName = 'sylius:exchange-rates:update';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Update exchange rates')
+            // bin/console s:e:u -> update all
+            // bin/console s:e:u USD_EUR UAH_EUR -> update only USD_EUR UAH_EUR if exists
+            // bin/console s:e:u --all USD_EUR UAH_EUR -> update all exclude USD_EUR UAH_EUR
+            // ??? or --include and --exclude options arrays
+            ->addArgument('pairs', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Exchange rates pairs, e.g. USD_UAH, if --all option enabled these pairs will be excluded')
+            ->addOption('all', true, InputOption::VALUE_NONE, 'Update all exchange rates. Default: true')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $exchangeRatesUpdaterService = new ExchangeRatesUpdater();
+        $io = new SymfonyStyle($input, $output);
+        $pairs = $input->getArgument('pairs');
+
+        if (count($pairs) > 0) {
+            $io->note(sprintf('You passed an argument: %s', implode(', ', $pairs)));
+        }
+
+        if ($input->getOption('all')) {
+            $io->note('You passed an option: all');
+        }
+
+        $io->success('Updated!');
+    }
+}
--- src/Service/ExchangeRatesUpdater.php
+++ src/Service/ExchangeRatesUpdater.php
@@ -6,5 +6,25 @@ namespace Acme\SyliusExchangeRatesUpdaterPlugin\Service;
 
 class ExchangeRatesUpdater
 {
-    
+
+    public function generateExchangeRates(array $pairs = []) : void
+    {
+        // collect all currencies
+        // for each unique pairs create enchange rate with 0 and update        
+    }
+
+    public function update() : void
+    {
+        // $exchangeRatesPairs = [];
+        // foreach($exchangeRatesPairs as $exchangeRatePair)
+        // {
+        //     $this->updateExchangeRate($exchangeRatePair);
+        // }
+    }
+
+    public function updateExchangeRate(array $pair = []) : void
+    {
+        // get rate by interface (?) loader
+        // update sylius rate
+    }
 }
